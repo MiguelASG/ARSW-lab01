1a023f5b27cdf87876168e6e14d7ceb043462708
Miguel Sanchez <miguel.sanchez-g@mail.escuelaing.edu.co>
2019-08-20 23:33:30 -0500

    ajustes

--- DOGS_RACE/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte2/src/main/java/arsw/threads/Galgo.java
+++ DOGS_RACE/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte2/src/main/java/arsw/threads/Galgo.java
@@ -26,7 +26,7 @@ public class Galgo extends Thread {
 			
 			if (paso == carril.size()) {						
 				carril.finish();
-				int ubicacion=regl.getUltimaPosicionAlcanzada();
+				int ubicacion= regl.getUltimaPosicionAlcanzada();
 				regl.setUltimaPosicionAlcanzada(ubicacion+1);
 				System.out.println("El galgo "+this.getName()+" llego en la posicion "+ubicacion);
 				if (ubicacion==1){
